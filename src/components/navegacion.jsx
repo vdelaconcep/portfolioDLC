55edd93a042fe00a254005456e4faa2413073046
Valeria <vdelaconcep@gmail.com>
2025-11-04 20:51:16 -0300

    Mergeado resolviendo el nav

--- src/components/navegacion.jsx
+++ src/components/navegacion.jsx
@@ -1,6 +1,14 @@
-import { motion } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Navegacion = () => {
+
+    const { scrollY } = useScroll();
+
+    const opacidadBg = useTransform(
+        scrollY,
+        [0, 100],
+        [0, 1]
+    );
 
     const simpleItems = [
         {
@@ -27,13 +35,22 @@ const Navegacion = () => {
         });
     };
 
+
     return (
-        <nav>
-            <ul className="list-none flex text-gray-300 text-lg font-medium">
+        <nav
+            className="fixed top-0 left-0 right-0 z-50 py-4 flex justify-center"
+            style={{
+                backgroundColor: useTransform(
+                    opacidadBg,
+                    (opacity) => `rgba(0, 0, 0, ${opacity})`
+                ),
+            }}>
+            <ul className="list-none flex text-gray-400 text-lg font-medium">
                 {simpleItems.map(item => 
                     <li
+                        key={item.linkto}
                         className="mx-2 last-of-type:mr-0 cursor-pointer hover:bg-pink-300 hover:text-gray-900 px-3 rounded-2xl"
-                        onClick={()=>{scrollToSection(item.linkto)}}>
+                        onClick={() => { scrollToSection(item.linkto); }}>
                         {item.titulo}
                     </li>
                 )}
